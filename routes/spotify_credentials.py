b6370c3e9ad9dd2d06224514e46b7541acad16e8
ruizmoisesl <123590176+ruizmoisesl@users.noreply.github.com>
2025-02-07 19:16:38 -0500

    Update spotify_credentials.py

--- routes/spotify_credentials.py
+++ routes/spotify_credentials.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyClientCredentials,SpotifyOAuth
 
 client_id= 'a4ef06c5cd894da8af06ecd1c77ad004'
 secret_client= 'b1ae99e13ba54f9c8b01063bce8c6aef'
-SPOTIPY_REDIRECT_URI = 'infofy.vercel.app/authorize'
+SPOTIPY_REDIRECT_URI = 'https://infofy.vercel.app/authorize'
 
 client_credentials_manager = SpotifyClientCredentials(client_id = client_id, client_secret = secret_client)
 sp = spotipy.Spotify(client_credentials_manager= client_credentials_manager)
